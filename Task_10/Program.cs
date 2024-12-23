a8355e111be34987dcc394b2ca83050fe4fa55c3
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:30:20 +0300

    Add project files.

---
+++ Task_10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    internal class Program
+    {
+        public static int[] Array = new int[9] { 5, 1, 66, 8, 2, 13, 9, 22, 45 };
+        public static int min = 0;
+        public static int max = 0;
+
+        //•	Задача 10: Напишите программу, которая
+        //выводит наибольшие и наименьшие числа из массива.
+        static void Main(string[] args)
+        {
+           
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if(max < Array[i])
+                {
+                    max = Array[i];
+                }
+            }
+            min = Array[0];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (min > Array[i])
+                {
+                    min = Array[i];
+                }
+            }
+            Console.WriteLine("Наименьшее число: " + min + " Наибольшее число: " + max);
+        }
+    }
+}
